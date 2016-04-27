fa1d54a6f05e05bf19a1ddae36c99dcabfae3fd5
Guillaume G <Guillaume G>
2016-04-27 20:22:50 +0200

    Init project structure (+ Poke)

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    compression = require('compression'),
+    i18n = require('i18n'),
+    routes = require('./routes'),
+    app = express();
+
+i18n.configure({
+  locales: ['en', 'fr'],
+  defaultLocale: 'en',
+  cookie: 'lang',
+  directory: path.join(__dirname + '/locales'),
+  updateFiles: false
+});
+
+app.set('port', process.env.PORT || 5500);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(i18n.init);
+
+app.use('/', routes());
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+} else {
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+
+  app.use(compression());
+}
+
+module.exports = app;
