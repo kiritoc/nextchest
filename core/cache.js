f1d7265d87ccb76a310fb7d5223bed2c17d2e4e3
Guillaume G <snake_eats@hotmail.com>
2016-05-06 17:11:12 +0200

    Add redis cache

---
+++ core/cache.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var cacheManager = require('cache-manager');
+var redisStore = require('cache-manager-redis');
+
+var redisCache = cacheManager.caching({
+    store: redisStore,
+    host: process.env.OPENSHIFT_REDIS_HOST || 'localhost',
+    port: process.env.OPENSHIFT_REDIS_PORT || 6379,
+    auth_pass: process.env.REDIS_PASSWORD || null,
+    db: 0,
+    ttl: 600
+});
+
+// listen for redis connection error event
+redisCache.store.events.on('redisError', function (error) {
+    // handle error here
+    console.log(error);
+});
+
+module.exports = redisCache;
